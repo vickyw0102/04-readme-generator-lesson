c0a5193bf044b7aef8054eed9486bb0c99210f97
vicky wong <vicky.boci@gmail.com>
2024-02-20 01:07:36 +0800

    npm install - done; added the inquirer and questions; updated the generateMarkdown file;

--- challenge/starter/index.js
+++ challenge/starter/index.js
@@ -3,17 +3,76 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+// const writeFile= utils.promisify(fs.writeFile);
+
 // array of questions for user
-const questions = [
+const questions = () => [
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is README title?',
+            name: 'Title',
+        },
+        {
+            type: 'input',
+            message: 'What is Table of Content?',
+            name: 'Table of Content',
+        },
+        {
+            type: 'input',
+            message: 'What is Description?',
+            name: 'Description',
+        },
+        {
+            type: 'input',
+            message: 'What is Installation?',
+            name: 'Installation',
+        },
+        {
+            type: 'input',
+            message: 'What is Usage?',
+            name: 'Usage',
+        },
+        {
+            type: 'input',
+            message: 'What is License?',
+            name: 'License',
+        },
+        {
+            type: 'input',
+            message: 'What is Contributing?',
+            name: 'Contributing',
+        },
+        {
+            type: 'input',
+            message: 'What is Tests?',
+            name: 'Tests',
+        },
+        {
+            type: 'input',
+            message: 'What is Questions?',
+            name: 'Questions',
+        },
+    ])
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    return fs.writeFile(path.join(process.cwd(), fileName), data);
+
+    // .then((response))=> writeFile ('README.md', response)
+        
+    }
+
+    // return writeFile(fileName, data);
+
 
 // function to initialize program
 function init() {
+    inquirer.prompt (questions).then((data) => {
+        writeToFile('README.md', generateMarkdown(data));
+    })
 
 }
 
--- challenge/starter/utils/generateMarkdown.js
+++ challenge/starter/utils/generateMarkdown.js
@@ -1,6 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.Description}
+
+  ## Table of Content
+
+  ${data.Table-of-Content}
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  -[Contributing](#contributing)
+  -[Tests](#tests)
+  -[Questions](#questions)
+
+  ## Installation
+  ${data.Installation}
+
+  ## Usage
+  ${data.Usage}
+
+  ## License
+  ${data.License}
+
+  ## Contributing
+  ${data.Contributing}
+
+  ## Tests
+  ${data.Tests}
+
+  ## Questions
+  ${data.Questions}
+  
+
 
 `;
 }
